3843c0622a92cd7ff061893102de909d4975b6be
AD8787 <75181003+AD8787@users.noreply.github.com>
2023-02-12 11:37:02 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,59 +1,85 @@
-let latitude = 22.7868542, longitude = 88.3643296;
+let latitude, longitude, destination;
 
-mapboxgl.accessToken = 'Insert Access Token Here';
+$(document).ready(function () {
+	alert("Please allow the device to know your location!")
+	initGeolocation();
+})
 
-var map = new mapboxgl.Map({
-	container: 'map',
-	style: 'mapbox://styles/mapbox/streets-v11',
-	center: [longitude, latitude],
-	zoom: 4
-});
+function initGeolocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(success);
+	}
+	else {
+		alert("Sorry, your browser does not support geolocation services.");
+	}
+}
 
-map.addControl(
-	new MapboxGeocoder({
-		accessToken: mapboxgl.accessToken,
-		mapboxgl: mapboxgl
+$(function () {
+	$("#navigate-button").click(function () {
+		window.location.href = `ar_weather.html?source=${latitude};${longitude}&destination=${destination[1]};${destination[0]}`
 	})
-);
-
-
-var img1 = document.querySelector("#amber")
-
-var marker1 = new mapboxgl.Marker({
-	element: img1
 })
-	.setLngLat([75.85133, 26.98547])
-	.addTo(map);
 
-var img2 = document.querySelector("#gateway")
+function success(position) {
+	longitude = position.coords.longitude;
+	latitude = position.coords.latitude
 
-var marker2 = new mapboxgl.Marker({
-	element: img2
-})
-	.setLngLat([72.835163, 18.920180])
-	.addTo(map);
+	mapboxgl.accessToken = 'Insert Access Token';
 
-var img3 = document.querySelector("#gate")
+	var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [longitude, latitude],
+		zoom: 4
+	});
 
-var marker3 = new mapboxgl.Marker({
-	element: img3
-})
-	.setLngLat([77.22931, 28.61495])
-	.addTo(map);
+	map.addControl(
+		new MapboxGeocoder({
+			accessToken: mapboxgl.accessToken,
+			mapboxgl: mapboxgl
+		}).on('result', function (e) {
+			destination = e.result.center
+		})
+	);
+
+	var img1 = document.querySelector("#amber")
+
+	var marker1 = new mapboxgl.Marker({
+		element: img1
+	})
+		.setLngLat([75.85133, 26.98547])
+		.addTo(map);
+
+	var img2 = document.querySelector("#gateway")
+
+	var marker2 = new mapboxgl.Marker({
+		element: img2
+	})
+		.setLngLat([72.835163, 18.920180])
+		.addTo(map);
+
+	var img3 = document.querySelector("#gate")
+
+	var marker3 = new mapboxgl.Marker({
+		element: img3
+	})
+		.setLngLat([77.22931, 28.61495])
+		.addTo(map);
 
 
-var img4 = document.querySelector("#lotus")
+	var img4 = document.querySelector("#lotus")
 
-var marker4 = new mapboxgl.Marker({
-	element: img4
-})
-	.setLngLat([77.25880, 28.553501])
-	.addTo(map);
+	var marker4 = new mapboxgl.Marker({
+		element: img4
+	})
+		.setLngLat([77.25880, 28.553501])
+		.addTo(map);
+	
+	var img5 = document.querySelector("#victoria")
 
-var img5 = document.querySelector("#victoria")
-
-var marker5 = new mapboxgl.Marker({
-	element: img5
-})
-	.setLngLat([88.342785, 22.546170])
-	.addTo(map);
+	var marker5 = new mapboxgl.Marker({
+		element: img5
+	})
+		.setLngLat([88.342785, 22.546170])
+		.addTo(map);
+}
